4b9d0a77f9a35674f55d51ed972a6f511d0e3699
zlr <543790767@qq.com>
2019-05-09 23:36:18 +0800

    addfunc

--- String/409longestPalindrome.cpp
+++ String/409longestPalindrome.cpp
@@ -13,7 +13,9 @@ public:
         int flag = 0, sum = 0;
         for(auto c : cmap){
             if(c.second%2 == 1)flag = 1;
+            //如果有落单的字符，则记录 
             sum += (c.second/2)*2; 
+            //没有的话，利用int的特性将数量floor为2的倍数 
         }
         return flag? sum + 1: sum;
     }
